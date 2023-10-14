ba387f19528f93d8bf117dd44b96de6085f767f8
Beda Schmid <beda@tukutoi.com>
2023-10-14 16:52:17 +0700

    Create plugin-name.php

---
+++ plugin-name.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The main plugin file
+ *
+ * @link    https://site.tld
+ * @since   1.0.0 Introduced on 2023-08-01 15:30
+ * @package Plugins\PluginName
+ * @author  Your Name <your-name@site.tld>
+ */
+
+/**
+ * The WordPress/Classicpress Plugin Header
+ *
+ * @see               https://developer.wordpress.org/plugins/plugin-basics/header-requirements/
+ *
+ * Plugin Name:       My Plugin Name
+ * Plugin URI:        https://site.tld/plugin-name-uri/
+ * Update URI:        https://github.com/smileBeda/github-wp-update
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Version:           1.0.0
+ * Author:            Ideally, your WordPress forum username.
+ * Requires at least: 5.5
+ * Requires PHP:      7.4
+ * Tested up to:      6.3
+ * Author URI:        https://site.tld/author-name-uri/
+ * License:           GPL-3.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.txt
+ * Text Domain:       plugin-name
+ * Domain Path:       /resources/languages
+ */
